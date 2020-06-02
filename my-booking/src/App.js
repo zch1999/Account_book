58a48f93792ec82ee516063fb38495125e90cbb4
zch1999 <1901394767@qq.com>
2020-06-02 23:58:07 +0800

    首页

--- my-booking/src/App.js
+++ my-booking/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import PriceList from './components/PriceList'
+
+const items = [
+  {
+    "id": 1,
+    "title": "去云南旅游",
+    "price": 200,
+    "date": "2020-05-14",
+    "category": {
+      "id": "1",
+      "name": "旅行",
+      "type": "outcome"
+    }
+  },
+  {
+    "id": 2,
+    "title": "去云南旅游",
+    "price": 400,
+    "date": "2020-05-14",
+    "category": {
+      "id": "1",
+      "name": "旅行",
+      "type": "outcome"
+    }
+  }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>欢迎来到天天记账簿</p>
       </header>
+      <PriceList 
+        items={items}
+        onModifyItem={(item) => {alert(item.id)}}
+        onDeleteItem={(item) => {alert(item.id)}}
+        />
     </div>
   );
 }
